07039879ae013e210379c243b1cb23e6fa27206f
EC2 Default User <ec2-user@ip-172-31-2-209.ap-northeast-1.compute.internal>
2023-05-22 15:58:04 +0000

    [modo]4-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
-  def show
 
+  def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
+    user = User.find(params[:id])
+  　unless user.id == current_user.id
+    redirect_to post_images_path
+ 　  end
+
     @user = User.find(params[:id])
   end
+
   def update
+
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+    redirect_to post_images_path
+
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path (@user.id)
-  end
+    end
 
 
   private
@@ -20,8 +31,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-
-
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
 
 end
 
